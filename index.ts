7629a38a56b8798d9bd6684b356af737cf38a90e
GLGIO7 <giovanelucas00@outlook.com>
2023-05-11 08:32:13 -0300

    refactor: javascript to typescript

--- index.ts
+++ index.ts
@@ -1,47 +1,68 @@
-let tela = document.getElementById("desktop");
-let pincel = tela.getContext("2d");
+let tela: HTMLCanvasElement = document.getElementById(
+	"desktop"
+) as HTMLCanvasElement;
 
-let telaMobile = document.getElementById("mobile");
-let pincelMobile = telaMobile.getContext("2d");
+let pincel: CanvasRenderingContext2D = tela.getContext(
+	"2d"
+) as CanvasRenderingContext2D;
+
+let telaMobile: HTMLCanvasElement = document.getElementById(
+	"mobile"
+) as HTMLCanvasElement;
+
+let pincelMobile: CanvasRenderingContext2D = telaMobile.getContext(
+	"2d"
+) as CanvasRenderingContext2D;
 
 let score = 0;
-const scoreCounter = document.querySelector(".score-board__counter");
+const scoreCounter: HTMLElement = document.querySelector(
+	".score-board__counter"
+) as HTMLElement;
 const settings = document.getElementById("settings");
 const refresh = document.getElementById("refresh");
 
-const toggleSettings = () =>
-	document.querySelector("ul").classList.toggle("active");
+const toggleSettings = () => {
+	const settings: HTMLUListElement = document.querySelector(
+		"ul"
+	) as HTMLUListElement;
+	settings.classList.toggle("active");
+};
 
 const refreshPage = () => window.location.reload();
 
-const easy = document.getElementById("easy");
-const normal = document.getElementById("normal");
-const hard = document.getElementById("hard");
-const custom = document.getElementById("custom");
+const easy: HTMLLIElement = document.getElementById("easy") as HTMLLIElement;
+const normal: HTMLLIElement = document.getElementById(
+	"normal"
+) as HTMLLIElement;
+const hard: HTMLLIElement = document.getElementById("hard") as HTMLLIElement;
+const custom: HTMLLIElement = document.getElementById(
+	"custom"
+) as HTMLLIElement;
 
-const valorCustom = () => {
-	let x = prompt(
-		"Digite um intervalo em ms (apenas números entre 99 e 2000). Exemplo: 0.5s = 500ms"
-	);
-	if (x > 99 && x <= 2000) {
+const valorCustom = (): number | undefined => {
+	let input: string =
+		prompt(
+			"Digite um intervalo em ms (apenas números entre 99 e 2000). Exemplo: 0.5s = 500ms"
+		) ?? "";
+	let x: number = parseInt(input);
+
+	if (x && x > 99 && x <= 2000) {
 		return x;
 	} else {
-		return alert("EITA, esse valor é inválido!");
+		alert("Valor é inválido!");
 	}
 };
-const desktopMode = document.getElementById("desktop");
-const mobileMode = document.getElementById("mobile");
 
 function desktopGame() {
-	desktopMode.style.display = "block";
-	mobileMode.style.display = "none";
+	tela.style.display = "block";
+	telaMobile.style.display = "none";
 	pincel.fillStyle = "rgba(0,0,0, 0.75)";
 	pincel.fillRect(0, 0, tela.width, tela.height);
-	let xAleatorio;
-	let yAleatorio;
+	let xAleatorio: number;
+	let yAleatorio: number;
 	let raio = 15;
 
-	function desenhaCirculo(x, y, raio, cor) {
+	function desenhaCirculo(x: number, y: number, raio: number, cor: string) {
 		pincel.fillStyle = cor;
 		pincel.beginPath();
 		pincel.arc(x, y, raio, 0, 2 * Math.PI);
@@ -53,12 +74,12 @@ function desktopGame() {
 		pincel.fillStyle = "rgba(0,0,0, 0.75)";
 		pincel.fillRect(0, 0, tela.width, tela.height);
 	}
-	function desenhaAlvo(x, y) {
+	function desenhaAlvo(x: number, y: number) {
 		desenhaCirculo(x, y, raio + 10, "#fff");
 		desenhaCirculo(x, y, raio, "#03fc18");
 	}
 
-	function geraPosicao(maximo) {
+	function geraPosicao(maximo: number) {
 		return Math.floor(Math.random() * maximo);
 	}
 
@@ -68,7 +89,7 @@ function desktopGame() {
 		yAleatorio = geraPosicao(tela.height);
 		desenhaAlvo(xAleatorio, yAleatorio);
 	}
-	function addScore(props) {
+	function addScore(props: MouseEvent) {
 		let x = props.pageX - tela.offsetLeft;
 		let y = props.pageY - tela.offsetTop;
 		if (
@@ -78,7 +99,7 @@ function desktopGame() {
 			y < yAleatorio + raio
 		) {
 			score += 10;
-			scoreCounter.innerHTML = score;
+			scoreCounter.innerHTML = score.toString();
 			limpaTela();
 		} else if (
 			x > xAleatorio - raio - 10 &&
@@ -87,7 +108,7 @@ function desktopGame() {
 			y < yAleatorio + raio + 10
 		) {
 			score += 5;
-			scoreCounter.innerHTML = score;
+			scoreCounter.innerHTML = score.toString();
 			limpaTela();
 		}
 	}
@@ -132,22 +153,22 @@ function desktopGame() {
 	normal.onclick = normalMode;
 	easy.onclick = easyMode;
 	custom.onclick = customMode;
-	settings.onclick = toggleSettings;
-	refresh.onclick = refreshPage;
+	settings!.onclick = toggleSettings;
+	refresh!.onclick = refreshPage;
 	tela.onclick = addScore;
 }
 
 function mobileGame() {
 	if (window.innerWidth < 1024) {
-		desktopMode.style.display = "none";
-		mobileMode.style.display = "block";
+		tela.style.display = "none";
+		telaMobile.style.display = "block";
 		pincelMobile.fillStyle = "rgba(0,0,0, 0.75)";
 		pincelMobile.fillRect(0, 0, telaMobile.width, telaMobile.height);
-		let xAleatorio;
-		let yAleatorio;
+		let xAleatorio: number;
+		let yAleatorio: number;
 		let raio = 10;
 
-		function desenhaCirculo(x, y, raio, cor) {
+		function desenhaCirculo(x: number, y: number, raio: number, cor: string) {
 			pincelMobile.fillStyle = cor;
 			pincelMobile.beginPath();
 			pincelMobile.arc(x, y, raio, 0, 2 * Math.PI);
@@ -159,12 +180,12 @@ function mobileGame() {
 			pincelMobile.fillStyle = "rgba(0,0,0, 0.75)";
 			pincelMobile.fillRect(0, 0, telaMobile.width, telaMobile.height);
 		}
-		function desenhaAlvo(x, y) {
+		function desenhaAlvo(x: number, y: number) {
 			desenhaCirculo(x, y, raio + 20, "#fff");
 			desenhaCirculo(x, y, raio, "#03fc18");
 		}
 
-		function geraPosicao(maximo) {
+		function geraPosicao(maximo: number) {
 			return Math.floor(Math.random() * maximo);
 		}
 
@@ -174,7 +195,7 @@ function mobileGame() {
 			yAleatorio = geraPosicao(telaMobile.height);
 			desenhaAlvo(xAleatorio, yAleatorio);
 		}
-		function addScore(props) {
+		function addScore(props: MouseEvent) {
 			let x = props.pageX - telaMobile.offsetLeft;
 			let y = props.pageY - telaMobile.offsetTop;
 			if (
@@ -184,7 +205,7 @@ function mobileGame() {
 				y < yAleatorio + raio
 			) {
 				score += 10;
-				scoreCounter.innerHTML = score;
+				scoreCounter.innerHTML = score.toString();
 				limpaTela();
 			} else if (
 				x > xAleatorio - raio - 10 &&
@@ -193,7 +214,7 @@ function mobileGame() {
 				y < yAleatorio + raio + 10
 			) {
 				score += 5;
-				scoreCounter.innerHTML = score;
+				scoreCounter.innerHTML = score.toString();
 				limpaTela();
 			}
 		}
@@ -238,8 +259,8 @@ function mobileGame() {
 		normal.onclick = normalMode;
 		easy.onclick = easyMode;
 		custom.onclick = customMode;
-		settings.onclick = toggleSettings;
-		refresh.onclick = refreshPage;
+		settings!.onclick = toggleSettings;
+		refresh!.onclick = refreshPage;
 		telaMobile.onclick = addScore;
 	}
 }
